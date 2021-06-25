e890ea6e6ce85fd3c5bdd5f2fac8c59c3df00f41
William Grohe <william.grohe@gmail.com>
2021-06-24 22:29:12 -0300

    Marcar perguntas como respondidas e destacadas

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,11 +1,14 @@
-import { useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
 import deleteImg from '../assets/images/delete.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
 
 import { Button } from '../components/Button'
 import { Question } from '../components/Question'
 import { RoomCode } from '../components/RoomCode'
+
 // import { useAuth } from '../hooks/useAuth'
 import { useRoom } from '../hooks/useRoom'
 import { database } from '../services/firebase'
@@ -18,15 +21,36 @@ type RoomParams = {
 
 export function AdminRoom(){
     // const { user } = useAuth();
+    const history = useHistory()
     const params = useParams<RoomParams>();
     const roomId = params.id;
 
     const { questions, title} = useRoom(roomId)
 
+    async function handleEndRoom(){
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date()
+        })
+
+        history.push('/');
+    }
+
     async function handleDeleteQuestion(questionId: string){
         if(window.confirm('Tem certeza que deseja remover a pergunta?')){
             await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
         }
+    }
+
+    async function handleCheckQuestionAsAnswered(questionId: string){
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true,
+        })
+    }
+
+    async function handleHighlightQuestion(questionId: string){
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true
+        })
     }
 
     return (
@@ -36,7 +60,7 @@ export function AdminRoom(){
                     <img src={logoImg} alt="Logo Letmeask"/>
                     <div>
                         <RoomCode code={roomId} />
-                        <Button isOutlined>Encerrar sala</Button>
+                        <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
                     </div>
                 </div>
             </header>
@@ -53,7 +77,25 @@ export function AdminRoom(){
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
+                                isAnswered={question.isAnswered}
+                                isHighlighted={question.isHighlighted}
                             >
+                                {!question.isAnswered && (
+                                    <>
+                                    <button
+                                        type="button"
+                                        onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                                    >
+                                        <img src={checkImg} alt="Marcar como respondida"/>
+                                    </button>
+                                    <button
+                                        type="button"
+                                        onClick={() => handleHighlightQuestion(question.id)}
+                                    >
+                                        <img src={answerImg} alt="Dar destaque à pergunta"/>
+                                    </button>
+                                    </>
+                                )}
                                 <button
                                     type="button"
                                     onClick={() => handleDeleteQuestion(question.id)}
